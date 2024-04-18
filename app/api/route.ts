3c3a3223f3439673c872253b94d3832daf798d34
jeremyperwira <jeremyperwira@protonmail.com>
2024-04-18 19:14:15 +0700

    Updating main branch

---
+++ app/api/route.ts
@@ -0,0 +1,4 @@
+// Catatan Ngab! : Route Handlers are only available inside the app directory. 
+// They are the  equivalent of API Routes inside the pages directory meaning you do not need to use API Routes and Route Handlers together.
+export const dynamic = 'force-dynamic' // defaults to auto
+export async function GET(request: Request) {}
