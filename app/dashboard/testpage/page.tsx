7eae3882c40b3a7f7350bf17731d92ed91af87a2
jeremyperwira <jeremyperwira@protonmail.com>
2024-04-20 13:48:15 +0700

    Off API

--- app/dashboard/testpage/page.tsx
+++ app/dashboard/testpage/page.tsx
@@ -1,37 +1,37 @@
-async function getData() {
-  const res = await fetch('http://localhost:3000/api/users/post')
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
+// async function getData() {
+//   const res = await fetch('http://localhost:3000/api/users/post')
+//   // The return value is *not* serialized
+//   // You can return Date, Map, Set, etc.
  
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
-  }
+//   if (!res.ok) {
+//     // This will activate the closest `error.js` Error Boundary
+//     throw new Error('Failed to fetch data')
+//   }
  
-  return res.json()
-}
+//   return res.json()
+// }
  
-export default async function Page() {
-  const data = await getData()
+// export default async function Page() {
+//   const data = await getData()
  
-  return <main>
-    <h1>Test API</h1>
-    <p>*Tergantung format API JSON kalo array/nested array pake .map()</p>
+//   return <main>
+//     <h1>Test API</h1>
+//     <p>*Tergantung format API JSON kalo array/nested array pake .map()</p>
     
-    <div>
-      {Object.keys(data.nasional).map((category) => (
-        <div className="" key={category}>
-          <h2 className="font-bold">{category}</h2>
-          <ul>
-            {Object.entries(data.nasional[category]).map(([product, details]) => (
-              <li key={product}>
-                <p>Product: {product}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </div>
+//     <div>
+//       {Object.keys(data.nasional).map((category) => (
+//         <div className="" key={category}>
+//           <h2 className="font-bold">{category}</h2>
+//           <ul>
+//             {Object.entries(data.nasional[category]).map(([product, details]) => (
+//               <li key={product}>
+//                 <p>Product: {product}</p>
+//               </li>
+//             ))}
+//           </ul>
+//         </div>
+//       ))}
+//     </div>
   
-  </main>
-}
+//   </main>
+// }
