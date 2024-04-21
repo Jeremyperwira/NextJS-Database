8fd9b300b4ef886477e5fc3ff1b52f2cc8e93b94
jeremyperwira <jeremyperwira@protonmail.com>
2024-04-21 20:25:23 +0700

    Added Typing animate

--- app/dashboard/(overview)/error.tsx
+++ app/dashboard/(overview)/error.tsx
@@ -20,7 +20,7 @@ export default function Error({
   return (
     
     <div className='space-y-2 bg-slate-100 h-full max-h-full flex flex-col justify-center items-center rounded-md'>
-      <h2 className='text-sm font-medium text-green-900 transition-colors'>Ups... Sepertinya ada Kesalahan!</h2>
+      <h2 className='animate-typing overflow-hidden text-sm font-medium text-green-900 transition-colors'>Ups... Sepertinya ada Kesalahan!</h2>
       <button  className=' 
       rounded-lg bg-green-900 px-3 py-2 text-sm font-medium text-white transition-colors transition ease-in-out hover:-translate-y-0.5 hover:scale-105 delay-200 duration-300 hover:bg-yellow-500 md:text-base'
         onClick={
@@ -31,6 +31,8 @@ export default function Error({
         Coba lagi
       </button>
     </div>
+
+    
     
   )
 }   
