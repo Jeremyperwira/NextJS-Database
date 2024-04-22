a1ff73f266d9456469d0b7fa589ea6754625d4c6
jeremyperwira <jeremyperwira@protonmail.com>
2024-04-23 00:56:02 +0700

    disable fetch test

--- app/dashboard/testpage/page.tsx
+++ app/dashboard/testpage/page.tsx
@@ -1,24 +1,24 @@
-async function getData() {
-  const res = await fetch('http://localhost:3000/api/users/post')
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
+// async function getData() {
+//   const res = await fetch('http://localhost:3000/api/users/post')
+//   // The return value is *not* serialized
+//   // You can return Date, Map, Set, etc.
  
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
-  }
+//   if (!res.ok) {
+//     // This will activate the closest `error.js` Error Boundary
+//     throw new Error('Failed to fetch data')
+//   }
  
-  return res.json()
-}
+//   return res.json()
+// }
  
-export default async function Page() {
-  const data = await getData()
+// export default async function Page() {
+//   const data = await getData()
  
-  return <main>
-    <h1>Test API</h1>
-    <p>*Tergantung format API JSON kalo array/nested array pake .map()</p>
-    </main>
-}
+//   return <main>
+//     <h1>Test API</h1>
+//     <p>*Tergantung format API JSON kalo array/nested array pake .map()</p>
+//     </main>
+// }
     
     
 
