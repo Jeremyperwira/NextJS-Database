32e5dfd668a24e59a7d0d0329bef9814a02c8952
jeremyperwira <jeremyperwira@protonmail.com>
2024-04-24 04:34:19 +0700

    *

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,12 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
 import { lusitana } from '@/app/ui/fonts';
+import { ClerkProvider } from '@clerk/nextjs'
+
 
 export default function LoginPage() {
   return (
+    
     <main className="flex items-center justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
         <div className="flex h-20 w-full items-end rounded-lg bg-green-900 p-3 md:h-36">
@@ -15,5 +18,6 @@ export default function LoginPage() {
         <LoginForm />
       </div>
     </main>
+    
   );
 }
--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,18 @@
-import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
+import {
+  clerkMiddleware,
+  createRouteMatcher
+} from '@clerk/nextjs/server';
  
-export default NextAuth(authConfig).auth;
+const isProtectedRoute = createRouteMatcher([
+  '/dashboard(.*)',
+  '/forum(.*)',
+]);
+ 
+export default clerkMiddleware((auth, req) => {
+
+  if (isProtectedRoute(req)) auth().protect();
+});
  
 export const config = {
-  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
